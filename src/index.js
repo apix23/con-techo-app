46afd76516b92bb1b469d17909b0775eae0f306f
Migracode <sanchezgalejandroj@gmail.com>
2020-12-03 23:51:10 +0100

    creation of the header component, downloading some JQuery and Popper.js dependencies for Bootstrap, right now the header is a 70% ready, it just left the hover effect on the buttons and some functionalities for the menu

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
