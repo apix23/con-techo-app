938af5f6ac4c7a8e623f9dde93f6ba10a5c62d1a
Migracode <sanchezgalejandroj@gmail.com>
2020-12-11 17:02:24 +0100

    commenting Bootstrap imports in all files

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+// //import "bootstrap/dist/css/bootstrap.min.css";
 import { Carousel } from "react-bootstrap";
 import img from "../src/img/apartamentos-noluna.jpg";
 
