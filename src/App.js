2ea6aade98c0c22c106bf3f608a9175f3d6041cd
Thony Nava <adriannnava@hotmail.com>
2020-12-12 23:40:23 +0100

    db

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import './App.css';
-import Header from './Components/Header';
-import HomePage from './HomePage';
-import AllComponents from './AllComponents';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header";
+import HomePage from "./HomePage";
+import AllComponents from "./AllComponents";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router className="App">
       <Header />
       <Switch>
-      <Route exact path="/">
-      <HomePage/>
-      </Route>
-      <Route path="/Components">
-      <AllComponents/>
-      </Route>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/Components" component={AllComponents} />
       </Switch>
     </Router>
   );
